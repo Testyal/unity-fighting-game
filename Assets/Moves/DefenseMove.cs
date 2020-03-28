056a5fad3f24ea0af0eff43f5a34a7784e7bc6ca
Billy <williamvrossii@gmail.com>
2020-03-28 08:44:58 +0000

    Create a DefenseMove class to attach behavior to a defending opponent without using the Move class.

---
+++ Assets/Moves/DefenseMove.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+
+
+class DefenseMove: MonoBehaviour
+{
+    [Header("Opponent frame data")] 
+    [SerializeField] public int onBlock;
+    [SerializeField] private int onHit;
+    
+    public (DefenseState, Action<MovementController>) InitializeDefenderBlock()
+    {
+        return (DefenseState.Blocking, controller => controller.DisableMotion());
+    }
+
+    public (DefenseState, Action<MovementController>) InitializeDefenderHit()
+    {
+        return (DefenseState.Hit, controller => controller.EnterLanding(JumpingDirection.Left));
+    }
+
+    private int elapsedDefenderFrames = 0;
+    public (DefenseState, Action<MovementController>) TickDefenderBlock()
+    {
+        elapsedDefenderFrames++;
+
+        if (elapsedDefenderFrames == onBlock)
+        {
+            Destroy(this.gameObject);
+            return (DefenseState.None, controller => controller.EnableMotion());
+        }
+
+        return (DefenseState.Blocking, _ => { });
+    }
+
+    public (DefenseState, Action<MovementController>) TickDefenderHit()
+    {
+        elapsedDefenderFrames++;
+
+        if (elapsedDefenderFrames == onHit)
+        {    
+            Destroy(this.gameObject);
+            return (DefenseState.None, controller => controller.EnableMotion());
+        }
+
+        return (DefenseState.Hit, _ => { });
+    }
+}
